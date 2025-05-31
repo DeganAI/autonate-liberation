ba600432a01231ede66596821afa1dbfee48430c
natefrog <wetakatini@gmail.com>
2025-05-30 23:41:45 -1000

    Create index.ts

---
+++ agents/autonate-prime/index.ts
@@ -0,0 +1,5 @@
+import { Agent, AgentConfig } from "@elizaos/core";
+import { autonateCharacter } from "../../characters/autonate-character";
+import { dialpadPlugin } from "../../plugins/dialpad-plugin";
+import { wellnessPlugin } from "../../plugins/wellness-plugin";
+import { tracking
